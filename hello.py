c1da638f30b6692cba2e5b9b2d02e1ebf8fdfbc3
VenkataramanaTedlapu123 <venkataramana.tedlapu@1tnc.de>
2025-07-30 15:15:44 +0530

    Test commit to add sample.py file to project

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
 
-BAMBOO_URL = "https://26e986c7a6b7.ngrok-free.app/rest/api/latest/queue/AUT-AUT"
+BAMBOO_URL = "https://d6b44a9e47fc.ngrok-free.app/rest/api/latest/queue/AUT-AUT"
 BAMBOO_USER = "krian"
 BAMBOO_PASS = "krian@1234"
 
