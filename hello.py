0cdf3ee1358c1fe66af2f372714241b6a8af25f5
VenkataramanaTedlapu123 <venkataramana.tedlapu@1tnc.de>
2025-08-04 11:22:47 +0530

    new changes made for commit new message PROJ-1

--- hello.py
+++ hello.py
@@ -80,4 +80,4 @@ def webhook():
     return jsonify({'message': 'Unhandled event type'}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(port=5000, debug=True)
